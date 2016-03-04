b6d80c34c22e7d8b3966828c6bd67538346a119d
Byron Lutz <byronlutz@gmail.com>
2016-03-04 00:27:12 -0800

    Built out second homepage block
    Including the blockquote styles

--- index.php
+++ index.php
@@ -10,8 +10,13 @@
 
 <div class='large-block' id='test-block-2'>
   <div class='large-block__content'>
-    <h2>Apply for UCLA UniCamp</h2>
-    <h4>Apps due: Friday 7th week<br />www.unicamp.org/apply</h4>
+    <h2>UCLA Student's Official Philanthropy</h2>
+    <h4>UniCamp is an indepently funded 501(c)3 that annually links 500 UCLA student volunteers with over 1,200 urban LA youth.</h4>
+    <a class='button button--homepage'>Learn More</a>
+    <blockquote class='large-block__blockquote'>
+      "Children have a right to a childhood filled with beauty, joy, adventure, and companionship. They will grow if the soil they are nurtured in is rich with experience, love and good examples."
+      <cite class='large-block__blockquote__cite'>Alan Dyer "A Sense of Adventure"</cite>
+    </blockquote>
   </div>
 </div>
 
