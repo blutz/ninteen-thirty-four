8dc9961ce221515210001b52d001779dfda2a2f6
Byron Lutz <byronlutz@gmail.com>
2016-03-05 17:51:53 -0800

    Added page title and favicon

--- header.php
+++ header.php
@@ -3,6 +3,11 @@
 <head>
   <meta charset='<?php bloginfo('charset'); ?>' />
   <meta name='viewport' content='width=device-width' />
+  <title><? bloginfo('name'); ?><? wp_title('|'); ?></title>
+  <link rel="icon" type="image/png" sizes="192x192"  href="<? echo get_template_directory_uri(); ?>/images/favicon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<? echo get_template_directory_uri(); ?>/images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="<? echo get_template_directory_uri(); ?>/images/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<? echo get_template_directory_uri(); ?>/images/favicon-16x16.png">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
