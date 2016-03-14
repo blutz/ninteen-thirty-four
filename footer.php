df41dcfa5096d6fe58e432fee4f23d39f54cb7a6
Byron Lutz <byronlutz@gmail.com>
2016-03-14 13:07:27 -0700

    Started styling footer

--- footer.php
+++ footer.php
@@ -1,61 +1,65 @@
-<footer class='row'>
-  <div class='medium-2 columns'>
-    <h3>About Us</h3>
-    <ul>
-      <li><a href='#'>Mission &amp; Vision</a></li>
-      <li><a href='#'>FAQs</a></li>
-      <li><a href='#'>Job Opportunities</a></li>
-      <li><a href='#'>Our Relationship with UCLA</a></li>
-    </ul>
-  </div>
-  <div class='medium-2 columns'>
-    <h3>How you can help</h3>
-    <ul>
-      <li><a href='#'>Donate</a></li>
-      <li><a href='#'>Volunteer</a></li>
-      <li><a href='#'>Become a Sponsor</a></li>
-      <li><a href='#'>Become a Partner Agency</a></li>
-    </ul>
-  </div>
-  <div class='medium-2 columns'>
-    <h3>Programs</h3>
-    <ul>
-      <li><a href='#'>Classic UniCamp</a></li>
-      <li><a href='#'>W.A.L.L.</a></li>
-      <li><a href='#'>UniCorps</a></li>
-      <li><a href='#'>C.L.I.M.B.</a></li>
-      <li><a href='#'>AcademiCamp</a></li>
-    </ul>
-  </div>
-  <div class='medium-2 columns end'>
-    <h3>Alumni</h3>
-    <ul>
-      <li><a href='#'>Get Involved</a></li>
-      <li><a href='#'>Special Events</a></li>
-      <li><a href='#'>Biff "E" Breeze Blog</a></li>
-    </ul>
-  </div>
-  <div class='footer__contact'>
-    <div class='footer__contact__address'>
-      <strong>UCLA UniCamp Office</strong><br />
-      900 Hilgard Ave. Suite 301<br />
-      Los Angeles, CA 90066<br />
+<footer>
+  <div class='row footer__menu'>
+    <div class='medium-2 columns'>
+      <h3>About Us</h3>
+      <ul class='footer__submenu'>
+        <li><a href='#'>Mission &amp; Vision</a></li>
+        <li><a href='#'>FAQs</a></li>
+        <li><a href='#'>Job Opportunities</a></li>
+        <li><a href='#'>Our Relationship with UCLA</a></li>
+      </ul>
     </div>
-    <div class='footer__contact__phone'>
-      310.208.8252 (UCLA)<br />
-      310.824.1948 (fax)
+    <div class='medium-2 columns'>
+      <h3>How you can help</h3>
+      <ul class='footer__submenu'>
+        <li><a href='#'>Donate</a></li>
+        <li><a href='#'>Volunteer</a></li>
+        <li><a href='#'>Become a Sponsor</a></li>
+        <li><a href='#'>Become a Partner Agency</a></li>
+      </ul>
+    </div>
+    <div class='medium-2 columns'>
+      <h3>Programs</h3>
+      <ul class='footer__submenu'>
+        <li><a href='#'>Classic UniCamp</a></li>
+        <li><a href='#'>W.A.L.L.</a></li>
+        <li><a href='#'>UniCorps</a></li>
+        <li><a href='#'>C.L.I.M.B.</a></li>
+        <li><a href='#'>AcademiCamp</a></li>
+      </ul>
+    </div>
+    <div class='medium-2 columns end'>
+      <h3>Alumni</h3>
+      <ul class='footer__submenu'>
+        <li><a href='#'>Get Involved</a></li>
+        <li><a href='#'>Special Events</a></li>
+        <li><a href='#'>Biff "E" Breeze Blog</a></li>
+      </ul>
     </div>
   </div>
-  <div class='footer__social'>
-    <span class='footer__social__title'>find us on social media:</span>
-    <ul>
-      <li><a href='#'>Facebook</a></li>
-      <li><a href='#'>Twitter</a></li>
-      <li><a href='#'>Instagram</a></li>
-      <li><a href='#'>YouTube</a></li>
-    </ul>
+  <div class='row'>
+    <div class='footer__contact'>
+      <div class='footer__contact__address'>
+        <strong>UCLA UniCamp Office</strong><br />
+        900 Hilgard Ave. Suite 301<br />
+        Los Angeles, CA 90066<br />
+      </div>
+      <div class='footer__contact__phone'>
+        310.208.8252 (UCLA)<br />
+        310.824.1948 (fax)
+      </div>
+    </div>
+    <div class='footer__social'>
+      <span class='footer__social__title'>find us on social media:</span>
+      <ul>
+        <li><a href='#'>Facebook</a></li>
+        <li><a href='#'>Twitter</a></li>
+        <li><a href='#'>Instagram</a></li>
+        <li><a href='#'>YouTube</a></li>
+      </ul>
+    </div>
   </div>
-  <div class='footer__legal'>
+  <div class='row footer__legal'>
     <p>
       <img src='#'> UCLA UniCamp is a University Camps Inc. Program
     </p>
