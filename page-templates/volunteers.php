8f24ac24979b7332f3cd1a76f8eecd344f34465f
Byron Lutz <byronlutz@gmail.com>
2016-04-04 00:12:01 -0700

    Added Volunteers page

---
+++ page-templates/volunteers.php
@@ -0,0 +1,92 @@
+<?php /* Template Name: Volunteers */ ?>
+<?php get_header(); ?>
+
+<section
+class='photo-block'
+style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/volunteers-alumni/VolunteersAlumni.Volunteers3.jpg")'>
+  <div class='photo-block__content'>
+    <h2>About UCLA UniCamp</h2>
+    <p class='explainer'>
+    UCLA UniCamp is the “Official Philanthropy of the Students of UCLA”. Started in 1934 by 11 UCLA Students. UniCamp is one of UCLA’s largest and longest running traditions. on the surface, UniCamp provides an outlet for UCLA Student Volunteers to give back to their community and make a difference in the lives of LA’s urban youth. And if you dig a little deeper you will find that UCLA Student Volunteers end up walking away having grown more from their experience than the campers may have.
+    </p>
+  </div>
+</section>
+
+<section class='row content-row'>
+  <div class='medium-12 columns'>
+    <h2>Reasons to do UCLA UniCamp</h2>
+    <h5>Rewarding Leadership Opportunities</h5>
+    <p class='explainer'>UCLA Student Volunteers form the backbone of UCLA UniCamp. Students are given a lot of responsibility and in turn are given the opportunity to grow leaps and bounds. It will be hard to find another experience as rewarding as volunteering for UCLA UniCamp.</p>
+    <h5>Forget about the stress of daily life and act like a kid again</h5>
+    <p class='explainer'>UCLA UniCamp allows every participant to act like a kid. Campers &amp; Volunteers. Joining UCLA UniCamp allows everyone to forget about their daily stresses for just a few hours a week and just act like a kid again.</p>
+    <h5>Have a place to call home</h5>
+    <p class='explainer'>Meet a small group of like-minded individuals who will quickly become your family at UCLA. The friends you make volunteering with UCLA UniCamp will be friends that you have for a lifetime.</p>
+  </div>
+</section>
+
+<section
+class='photo-block'
+style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/volunteers-alumni/VolunteersAlumni.Volunteers4.jpg")'>
+  <div class='photo-block__content'>
+    <h2>UniCamp's Volunteers</h2>
+    <p class='explainer'>
+      UCLA UniCamp has approximately 500 UCLA Student Volunteers every year. Volunteers are recruited in Winter Quarter (February/ March) and spend the Spring training and bonding with their session. If you are interested in becoming a UCLA UniCamp volunteer learn more below:
+    </p>
+    <div class='expand expand--collapsed'>
+      <a href='#' class='button expand__button'>
+        More <i class='expand__button__icon'></i>
+      </a>
+      <div class='expand__content'>
+        <p class='explainer explainer--left'>
+          <strong>UCLA UniCamp Volunteer Expectations:</strong>
+        </p>
+        <ul class='explainer explainer--left'>
+          <li>Current UCLA student or Alumni UCLA UniCamp camper</li>
+          <li>At least 18 years of age</li>
+          <li>$500 fundraising goal</li>
+          <li>Attend weekly training meetings at UCLA during Spring Quarter</li>
+        </ul>
+        <p class='explainer'>
+          Ready to become a Woodsey and join the UCLA UniCamp family? Volunteer recruitment usually begins in February of each year. Check back in January to receive more information on recruitment as it is released.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section
+class='photo-block'
+style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/volunteers-alumni/VolunteersAlumni.Woodsey.jpg")'>
+  <div class='photo-block__content'>
+    <h2>What is Woodsey?</h2>
+    <p class='explainer'>
+      The term Woodsey has come to mean a lot of things at UCLA UniCamp and most importantly it means family. When you are walking around campus and you see someone wearing a shirt that says Woodsey, you know that they are part of the UniCamp family. Even if you don’t know them you could stop them and talk to them about their session and ask how the kids were, and you can be guaranteed that they will stop and chat with you.
+    </p>
+    <div class='expand expand--collapsed' data-expand='whatIsWoodseyExpand'>
+      <a class='button expand__button'>
+        More <i class='expand__button__icon'></i>
+      </a>
+    </div>
+  </div>
+</section>
+<section class='expand-block' id='whatIsWoodseyExpand'>
+  <div class='row'>
+    <div class='medium-12 columns'>
+      <p class='explainer'>
+        <strong>Woodsey</strong> \wůd-sē\ <em>n, always cap</em>: a person, current or alumnus Student Volunteer of the UniCamp program <em>(traditionally classified as a new Woodsey if it is the Student Volunteer’s first year in the program; classified as an old Woodsey once the volunteer has attended a UniCamp session.)</em>
+      </p>
+      <p class='explainer'>
+        <strong>woodsey</strong> \wůd-sē\ <em>adj</em> 1: of, relating to anything that has to deal with UniCamp [i.e. - a woodsey event] 2: of, relating to something that coincides with the mission and/or atmosphere of UniCamp [i.e. - a woodsey word, a woodsey attitude] <em>(often used in conjunction with non non-woodsey to indicate something that does not coincide with the mission and/ or atmosphere of UniCamp) [a list of non-woodsey items: a bad attitude, alcohol, drugs, cigarettes, junk food, candy, profanity, NSD’s, poop-outs, cell phones, video games, all beauty products, scented lotions, perfume, revealing clothes, bikinis…]</em>
+      </p>
+      <p class='explainer'>
+        <strong>woodsey</strong> \wůd-sē\ <em>vb</em>: the act of being a Woodsey [i.e. – using only appropriate language, being in the mountains miles away from civilization, acting as a role model, singing songs and playing games, preparing for your week at camp, fundraising to help send kids to camp, helping inspire Today’s UniCamp Camper to be Tomorrow’s Student Volunteer]
+      </p>
+      <hr />
+      <p class='explainer'>
+        The term Woodsey has come to mean a lot of things at UCLA UniCamp and most importantly it means family. When you are walking around campus and you see someone wearing a shirt that says Woodsey, you know that they are part of the UniCamp family. Even if you don’t know them you could stop them and talk to them about their session and ask how the kids were, and you can be guaranteed that they will stop and chat with you.
+      </p>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
