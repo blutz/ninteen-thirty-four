f8b97b4787901e5e4302fc76b85561a2dc3a31a3
Byron Lutz <byronlutz@gmail.com>
2016-04-18 04:25:48 -0700

    Added a non-terrible 404 page #7

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<section
+class='photo-block'
+style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/campers-families/CampersFamilies.WhyCamp.jpg")'>
+  <img class='photo-block__small-image' src='<? echo get_template_directory_uri(); ?>/images/photos/campers-families/CampersFamilies.WhyCamp.jpg' />
+  <div class='photo-block__content'>
+    <h2>Page not found</h2>
+    <p class='explainer'>Sorry, we couldn't find the page you're looking for (404). If you think this is an error, please let us know.</p>
+  </div>
+</section>
+
+<?php get_footer(); ?>
