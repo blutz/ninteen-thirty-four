5722045ac07763fb934a67607d61cd1f6f7cdf9e
Byron Lutz <byronlutz@gmail.com>
2016-11-01 01:19:21 -0700

    Add HC/OCP applications

---
+++ page-templates/apply-volunteer.php
@@ -0,0 +1,61 @@
+<?php /* Template Name: Volunteer application */ ?>
+<?php get_header(); ?>
+
+<meta name='unicamp-page' value='camper-application' />
+
+<section
+class='photo-block'
+style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/campers-families/CampersFamilies.WhyCamp.jpg")'>
+  <img class='photo-block__small-image' src='<? echo get_template_directory_uri(); ?>/images/photos/campers-families/CampersFamilies.WhyCamp.jpg' />
+  <div class='photo-block__content'>
+    <h2>OCP/HC Volunteer Applications</h2>
+    <p class='explainer'>Head Counselor and Older Camper Program volunteer applications are now open!</p>
+  </div>
+</section>
+
+<section class='row content-row'>
+  <div class='medium-10 medium-offset-1 columns'>
+    <p class='explainer'>Volunteer applications for Head Counselor and OCP (CLIMB, UniCorps, and WALL) are now open and are due on Friday, November 4. Classic camp volunteer applications will be released Winter Quarter — look for us on Bruinwalk! Please note that all volunteers must be UCLA students/alumni or former UniCamp campers.</p>
+    <div class='row'>
+      <div class='medium-6 columns'>
+        <h5>CLIMB Advisor</h5>
+        <p>
+          <strong>Due:</strong> Friday, November 4 at 11:59 p.m.
+          <br />
+          <strong>Contact:</strong> Email CLIMB PDs at <a href='mailto:climbpds@unicamp.org'>climbpds@unicamp.org</a>.
+        </p>
+        <a class='button' href='http://r.unicamp.org/climb'>Download application &rarr;</a>
+
+        <h5>WALL Advisor</h5>
+        <p>
+          <strong>Due:</strong> Friday, November 4 at 11:59 p.m.
+          <br />
+          <strong>Contact:</strong> Email WALL PDs at <a href='mailto:wallpds@unicamp.org'>wallpds@unicamp.org</a>.
+        </p>
+        <a class='button' href='http://r.unicamp.org/wall'>Download application &rarr;</a>
+
+        <h5>UniCorps Advisor</h5>
+        <p>
+          <strong>Due:</strong> Friday, November 4 at 9:00 p.m.
+          <br />
+          <strong>Contact:</strong> Email UniCorps PDs at <a href='mailto:ucpds@unicamp.org'>ucpds@unicamp.org</a>.
+        </p>
+        <a class='button' href='http://r.unicamp.org/unicorps'>Download application &rarr;</a>
+      </div>
+      <div class='medium-6 columns'>
+        <h5>Head Counselor (HC)</h5>
+        <p>
+          <strong>Due:</strong> Friday, November 4 at 11:59 p.m.
+          <br />
+          <strong>Contact:</strong> Email Blitzen at <a href='mailto:blutz@unicamp.org'>blutz@unicamp.org</a>.
+        </p>
+        <a class='button' href='http://r.unicamp.org/hc'>Apply online &rarr;</a>
+        <p>
+          <em>This position is different from the "Head Counselor" position from past years and there will NOT be an HCA position this year. This is the last LSHIP application. <a href='http://r.unicamp.org/leadership2017'>Click here for more information about the new structure.</a></em>
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
