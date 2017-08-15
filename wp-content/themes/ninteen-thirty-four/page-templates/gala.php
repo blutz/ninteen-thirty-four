1aeb7eda71155cd0a9f7294251bfe10fa41dd1f4
Byron Lutz <byronlutz@gmail.com>
2017-08-14 17:38:03 -0700

    Update gala page for 2017 gala

--- wp-content/themes/ninteen-thirty-four/page-templates/gala.php
+++ wp-content/themes/ninteen-thirty-four/page-templates/gala.php
@@ -17,23 +17,22 @@
     <div class='large-6 large-offset-3 medium-8 medium-offset-2 columns'>
       <div id='main-heading'>
         <h2 class='supporting-h2'>UCLA UniCamp presents:</h2>
-        <h2 class='main-h2'>The 2<sup>nd</sup> Annual Campfire Gala</h2>
-        <h2 class='supporting-h2'>Saturday May 7, 2016</h2>
+        <h2 class='main-h2'>The 3<sup>rd</sup> Annual Campfire Gala</h2>
+        <h2 class='supporting-h2'>Saturday, October 14, 2017</h2>
       </div>
       <hr />
       <ul class='button-list'>
-        <li><a href='<?php echo get_template_directory_uri(); ?>/sites/gala/starfish-story.jpg' target='_blank'><span>Starfish Story</span></a></li>
-        <li><a href='http://www.eventbrite.com/e/the-campfire-gala-tickets-21688903093?aff=ehomesaved' target='_blank'><span>Tickets</span></a></li>
-        <li><a href='http://goo.gl/forms/lbrD8IAU1q'><span>Woodsey Spirit Nomination</span></a></li>
-        <li><a href='<?php echo get_template_directory_uri(); ?>/sites/gala/Campfire_Gala_Sponsor.Auction_Packet.pdf'><span>Sponsorship</span></a></li>
+        <!--<li><a href='http://www.eventbrite.com/e/the-campfire-gala-tickets-21688903093?aff=ehomesaved' target='_blank'><span>Tickets</span></a></li> -->
+        <!--<li><a href='<?php echo get_template_directory_uri(); ?>/sites/gala/Campfire_Gala_Sponsor.Auction_Packet.pdf'><span>Sponsorship</span></a></li>-->
         <li><a href='https://goo.gl/photos/pXfJ9aQuhJ5xanqG8'><span>Photos</span></a></li>
+        <!--<li><a href='#' class='button-list__disabled'><span>About the<br />Honoree</span></a></li>-->
       </ul>
       <hr />
-      <p>Last day to buy tickets at discounted rate is <strong class='very-strong'>April 29<sup>th</sup> 2016</strong></p>
+      <p>Very limited Early Bird tickets are available for purchase online only until <strong>September 1<sup>st</sup> or until sold out</strong>.</p>
       <p>Reception &ndash; 5 pm<br />Dinner &ndash; 7pm<p>
-      <p><strong>UCLA Pauley Pavilion</strong><br />301 Westwood Plaza, Los Angeles, CA 90095</p>
+      <p><strong>UCLA Carnesale Commons</strong><br />251 Charles E Young Drive West, Los Angeles, CA 90095</p>
       <p>Cocktail Attire Requested</p>
-      <p><small><strong>For more information or to purchase tickets by check, contact, <a href='mailto:events@unicamp.org'>events@unicamp.org</strong></small></p>
+      <p><small><strong>For more information, contact <a href='mailto:events@unicamp.org'>events@unicamp.org</strong></small></p>
     </div><!-- /cols -->
   </div><!-- /row -->
 </body>
