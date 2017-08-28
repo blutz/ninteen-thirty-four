7ba93d666d67c1b8fca978d8e693660a2e79427f
Byron Lutz <byronlutz@gmail.com>
2017-08-28 13:27:03 -0700

    Update leadership page

--- wp-content/themes/ninteen-thirty-four/page-templates/leadership.php
+++ wp-content/themes/ninteen-thirty-four/page-templates/leadership.php
@@ -18,32 +18,108 @@ style='background-image: url("<? echo get_template_directory_uri(); ?>/images/ph
     </p>
 
     <br />
+    <div class='row'>
+      <div class='medium-6 columns'>
+        <h4>UniCabinet</h4>
+        <p>
+          Even after 83 years, we’re never done improving. This year, we will be starting training earlier and giving students more resources and opportunities to learn and fundraise. UCLA UniCamp will be led by UniCabinet — a new student leadership group that will give students the space and support to envision the future of the UCLA UniCamp programs and turn it into a reality.
+        </p>
 
-    <h4>UniCabinet</h4>
-    <p class='explainer'>
-      Even after 83 years, we’re never done improving. This year, we will be starting training earlier and giving students more resources and opportunities to learn and fundraise. UCLA UniCamp will be led by UniCabinet — a new student leadership group that will give students the space and support to envision the future of the UCLA UniCamp programs and turn it into a reality.
-    </p>
+        <a class='button' href='#cabinet'>Apply for UniCabinet &rarr;</a>
+        <p>
+          <small>
+          <strong>UniCabinet is open to all current UCLA Students, regardless of experience. The application will be open until Tuesday, September 5 at 11:59pm.</strong>
+          UCLA UniCamp prides itself in being the official charity of the students of UCLA, and a student leadership development program. We will provide training, mentoring, and tools; our only requirement for applicants is a passion for the mission.
+          </small>
+        </p>
+      </div>
+      <div class='medium-6 columns'>
+        <h4>Session Leadership (LSHIP)</h4>
+        <p>
+          Each session’s LSHIP team will be supported by UniCabinet. They will be given more resources and space to focus on working with their partner and student volunteers to develop a unique summer camp experience tailored to meet the needs of their chosen target population. 
+        </p>
 
-    <a class='button' href='#cabinet'>Apply for UniCabinet &rarr;</a>
-    <p class='explainer'>
-      <small>
-      <strong>UniCabinet is open to all current UCLA Students, regardless of experience. The application will be open until Tuesday, September 5 at 11:59pm.</strong>
-      UCLA UniCamp prides itself in being the official charity of the students of UCLA, and a student leadership development program. We will provide training, mentoring, and tools; our only requirement for applicants is a passion for the mission.
-      </small>
-    </p>
+        <a class='button' href='#lship'>Apply for LSHIP &rarr;</a>
+        <p>
+          <small>
+          <strong>LSHIP is open to all Woodsey volunteers. The application will be open until Tuesday, September 5 at 11:59pm.</strong>
+          UCLA UniCamp prides itself in being the official charity of the students of UCLA, and a student leadership development program. We will provide training, mentoring, and tools; our only requirement for applicants is a passion for the mission.
+          </small>
+        </p>
+      </div>
+    </div>
 
-    <h4>Session Leadership (LSHIP)</h4>
-    <p class='explainer'>
-      Each session’s LSHIP team will be supported by UniCabinet. They will be given more resources and space to focus on working with their partner and student volunteers to develop a unique summer camp experience tailored to meet the needs of their chosen target population. 
-    </p>
+    <h4>Watch our presentation...</h4>
+    <div class='row'>
+      <div class='medium-6 medium-offset-3 columns'>
+        <script async class="speakerdeck-embed" data-id="8c15616659c44459854c2123e5b5c04d" data-ratio="1.33333333333333" src="//speakerdeck.com/assets/embed.js"></script>
+      </div>
+    </div>
+  </div>
+</section>
 
-    <a class='button' href='#lship'>Apply for LSHIP &rarr;</a>
-    <p class='explainer'>
-      <small>
-      <strong>LSHIP is open to all Woodsey volunteers. The application will be open until Tuesday, September 5 at 11:59pm.</strong>
-      UCLA UniCamp prides itself in being the official charity of the students of UCLA, and a student leadership development program. We will provide training, mentoring, and tools; our only requirement for applicants is a passion for the mission.
-      </small>
-    </p>
+<section class='row content-row'>
+  <div class='medium-12 columns'>
+    <h2>UniCabinet</h2>
+    <div class='row'>
+      <div class='medium-8 columns'>
+        <p><strong>UniCabinet</strong> is a group of UCLA students that will be the face and leadership of UCLA UniCamp, the student group.</p>
+        <p>Since these are student positions, UniCabinet will be given guidance, support, and training from professional staff and alumni (through the Program Advisory Board). Experience and knowledge aren't requirements; we only expect you to have a passion for UCLA UniCamp and a desire to learn.</p>
+        <p>More detailed information about each of these positions is available in the presentation above.</p>
+        <hr />
+        <p><strong>UCLA UniCamp Student Chair</strong> is the person who represents UCLA UniCamp. They envision and uphold UniCamp outcomes and missions through organizational strategic planning. They are also an important liason between the UCLA campus, UCLA UniCamp, and University Camps Inc.</p>
+        <p><strong>Partnerships Committee</strong> is led by the <strong>Chair of Partnerships</strong> with multiple <strong>vice-chairs</strong>. They oversee all <em>external</em> relationships including relationships with the broader UCLA and Los Angeles communities, organizational partners, camper families, and more.</p>
+        <p><strong>Volunteer Committee</strong> is led by the <strong>Chair of Volunteers</strong> with multiple <strong>vice-chairs</strong>. They oversee all <em>internal</em> operations. This includes Leadership Training (after the first year), all-camp volunteer training (e.g. Fall quarter training, retreats, etc.), supporting session training/programming, and more.</p>
+        <a class='button' href='#cabinet'>Apply for UniCabinet Online &rarr;</a>
+      </div>
+      <div class='medium-4 columns'>
+        <img src='<? echo get_template_directory_uri(); ?>/images/unicabinet/unicabinet-hierarchy.png' style='width: 100%' />
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class='row content-row'>
+  <div class='medium-10 medium-offset-1 columns'>
+    <h2>Session Leadership</h2>
+    <p>Each session is led by a Session Program Director. Throughout LSHIP training, Head Counselors can apply to be part of a SPD’s team. LSHIP is the only group that cannot be a Classic Camp/OCP volunteer or a member of UniCabinet.</p>
+    <p>The interview process for LSHIP is:</p>
+    <ol>
+      <li>Online application (<a href='#lship'>Apply here!</a>)</li>
+      <li>Initial interviews</li>
+      <li>Acceptance as an LSHIP Candidate</li>
+      <li>Summer/Fall Leadership Training</li>
+      <li><strong>(SPDs only)</strong> Development of a camp program that includes a partner, funding, and campers<br /><strong>(HCs only)</strong> Acceptance to an initiative's leadership team</li>
+    </ol>
+    <p>Once accepted as an HC/SPD Candidate, you'll be part of Leadership Training and will be working with UniCabinet, professional staff, and alumni (through the Program Advisory Board) to bring your initiative from idea to reality. For some initiatives, this process can take more than one year.</p>
+  </div>
+</section>
+
+<section class='row content-row'>
+  <div class='medium-12 columns'>
+    <h2>2017-2018 Calendar</h2>
+    <div class='row'>
+      <div class='medium-10 medium-offset-1 columns'>
+        <img src='<? echo get_template_directory_uri(); ?>/images/unicabinet/unicamp-calendar-online.png' style='width: 100%' />
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class='row content-row'>
+  <div class='medium-12 columns'>
+    <h2>Other Ways to Get Involved</h2>
+    <div class='row'>
+      <div class='medium-6 columns'>
+        <p><strong>Program Advisory Board (PAB)</strong> &mdash; PAB is composed of alumni who will have the responsibility of mentoring UniCabinet and providing resources and advice to current student leadership. If you’re interested in joining, email <a href='mailto:pab@unicamp.org'>pab@unicamp.org</a>.</p>
+        <p><strong>Classic Camp Volunteer</strong> &mdash; Applications for volunteers will be released at the start of the 2017-2018 school year. Keep an eye out on Bruinwalk!</p>
+        <p><strong>OCP PD/Advisor</strong> &mdash; More information about Older Camper Programs will be released in the next few weeks. Email the current OCP Program Directors for more information &mdash; contact CLIMB PDs at <a href='mailto:climbpds@unicamp.org'>climbpds@unicamp.org</a>, contact UniCorps PDs at <a href='mailto:ucpds@unicamp.org'>ucpds@unicamp.org</a>, and contact WALL PDs at <a href='mailto:wallpds@unicamp.org'>wallpds@unicamp.org</a>.</p>
+      </div>
+      <div class='medium-6 columns'>
+        <p><strong>Camp River Glen Summer Seasonal Resident Camp Staff Positions</strong> &mdash; These are paid internship positions including program positions, food service, housekeeping, and site operations. Contact the professional staff at <a href='mailto:wwirick@unicamp.org'>wwirick@unicamp.org</a> and <a href='mailto:jliou@unicamp.org'>jliou@unicamp.org</a>.</p>
+        <p><strong>Donate</strong> &mdash; Even if you’re not able to volunteer, you can help us improve our program by <a href='https://www.giveffect.com/charities/731-ucla-unicamp'>donating online</a> or by <a href='/gala'>attending our gala in October</a>.
+      </div>
+    </div>
   </div>
 </section>
 
@@ -64,5 +140,31 @@ style='background-image: url("<? echo get_template_directory_uri(); ?>/images/ph
     </div>
   </div>
 </section>
+
+<section class='row content-row'>
+  <div class='medium-12 columns'>
+    <h2>FAQs</h2>
+    <div class='row'>
+    <p>
+      <strong>Can I be a camp counselor and a UniCabinet member?</strong>
+      <br />
+      Yes! We think it’s valuable for all volunteers in UniCamp to interact with campers. UniCabinet members are encouraged to also be classic camp volunteers.
+    </p>
+
+    Why is UniCabinet only open to current UCLA students?
+    UCLA UniCamp is the official charity of the students of UCLA. As a student group, one of its primary missions is to give students the opportunity to grow, serve, and lead. Alumni and other Woodseys who don’t attend UCLA can still serve important roles elsewhere in the organization.
+
+    What if I can’t make it to leadership training?
+    Leadership training (DATES) is an important part of becoming part of UniCabinet or LSHIP. We will help you find transportation and housing in Westwood during the training. If you absolutely cannot make it, let us know on your application and we’ll reach out and try to work something out on a case-by-case basis.
+
+    Does this mean training meetings every week for the whole year!?
+    Nope! A big motivation behind these changes is to spread out training rather than concentrate it in one intense quarter. Volunteers will likely be asked to attend events roughly every 2-3 weeks throughout the year rather than once or twice a week for one quarter. We know life can get hectic throughout the quarter, so UniCabinet will be working on ways to make the time commitment easier for volunteers.
+
+    What if I have suggestions or questions?
+    Please email the Program Advisory Board at pab@unicamp.org — we’d love to hear your feedback!
+
+  </div>
+</section>
+
 
 <?php get_footer(); ?>
