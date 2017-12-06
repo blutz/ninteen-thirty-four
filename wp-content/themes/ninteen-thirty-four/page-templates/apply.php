f9e4e9627671a9288f14c79932d10276c8c340ca
Byron Lutz <byronlutz@gmail.com>
2017-12-05 21:34:24 -0800

    Updated deadline for new/old woodsey apps

--- wp-content/themes/ninteen-thirty-four/page-templates/apply.php
+++ wp-content/themes/ninteen-thirty-four/page-templates/apply.php
@@ -16,37 +16,8 @@ style='background-image: url("<? echo get_template_directory_uri(); ?>/images/ph
 <section class='row content-row'>
   <div class='medium-12 columns'>
     <h5>Classic Camp</h5>
-    <p class='explainer'>Be a camp counselor this summer with UCLA UniCamp! New volunteer applications are due November 30 and returning volunteer applications are due November 15.</p>
-    <p class='explainer'>Look out for us on Bruinwalk or email us at <a href='mailto:unicabinet@unicamp.org'>unicabinet@unicamp.org</a> to learn more.</p>
+    <p class='explainer'>Be a camp counselor this summer with UCLA UniCamp! Applications will be accepted on a rolling basis. Email us at <a href='mailto:unicabinet@unicamp.org'>unicabinet@unicamp.org</a> to learn more.</p>
     <a class='button' href='https://docs.google.com/forms/d/e/1FAIpQLSe_C-X9T38gxSzyTp40DKXGu1_0FQF7nrIthjnwJSobqXBHjw/viewform?c=0&w=1' style='display: inline-block; margin: auto; font-size: 1.3rem; text-align: center; width: auto; position: relative; left: 50%; transform: translateX(-50%); '>Apply online! &rarr;</a>
   </div>
 </section>
-<section class='row content-row'>
-  <div class='medium-12 columns'>
-    <h5>Older Camper Programs</h5>
-    <p class='explainer'>Applications to become an Older Camper Program advisor (for returning volunteers) are now available. Click the links below to apply for C.L.I.M.B., UniCorps, or W.A.L.L.<a href='/programs/#ocp'>Learn more about our Older Camper Programs</a>.</p>
-  </div>
-  <div class='medium-4 columns'>
-    <img src='<?php echo get_template_directory_uri(); ?>/images/logos/climb-logo.jpg' style='width: 50%; display: block; clear: both; margin: auto;'>
-    <strong>Core Lessons in Mind &amp; Body</strong>
-    <p>If you have any questions about the application or interview process, email <a href='climbpds@unicamp.org'>climbpds@unicamp.org</a></p>
-    <p>The C.L.I.M.B. application is due by 11:59 p.m. on Thursday, November 16.</p>
-    <a class='button' href='https://docs.google.com/document/d/1o_ZNy5CtLAKBYnGAnhhwK0-OhH8I76BLNRL6VYNzbxk/edit'>Apply for C.L.I.M.B. &rarr;</a>
-  </div>
-  <div class='medium-4 columns'>
-    <img src='<?php echo get_template_directory_uri(); ?>/images/logos/unicorps-logo.jpg' style='width: 50%; display: block; clear: both; margin: auto;'>
-    <strong>UniCorps</strong>
-    <p>If you have any questions about the application or interview process, email <a href='climbpds@unicamp.org'>ucpds@unicamp.org</a></p>
-    <p>The UniCorps application is due by 11:30 p.m. on Wednesday, November 15.</p>
-    <a class='button' href='https://docs.google.com/document/d/1XfqBoCWcT52oFKzttkkFdxNfPh48QVT82FZvU11RWlA/edit'>Apply for UniCorps &rarr;</a>
-  </div>
-  <div class='medium-4 columns'>
-    <img src='<?php echo get_template_directory_uri(); ?>/images/logos/wall-logo.jpg' style='width: 50%; display: block; clear: both; margin: auto;'>
-    <strong>Wilderness Adventures in Leadership and Learning</strong>
-    <p>If you have any questions about the application or interview process, email <a href='climbpds@unicamp.org'>wallpds@unicamp.org</a></p>
-    <p>The W.A.L.L. application is due by 11:30 p.m. on Wednesday, November 15.</p>
-    <a class='button' href='https://docs.google.com/document/d/1lnXTkGfOTT-o89csGzzXE1bAfKmwojnrxDJtBfvXd2I/edit'>Apply for W.A.L.L. &rarr;</a>
-  </div>
-</section>
-
 <?php get_footer(); ?>
