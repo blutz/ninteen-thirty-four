6a1efe931fdc347303309aefc8f5a74e12a1e253
Byron Lutz <byronlutz@gmail.com>
2018-01-23 00:03:31 -0800

    Updated application link

--- wp-content/themes/ninteen-thirty-four/page-templates/apply.php
+++ wp-content/themes/ninteen-thirty-four/page-templates/apply.php
@@ -17,7 +17,7 @@ style='background-image: url("<? echo get_template_directory_uri(); ?>/images/ph
   <div class='medium-12 columns'>
     <h5>Classic Camp</h5>
     <p class='explainer'>Be a camp counselor this summer with UCLA UniCamp! Applications will be accepted on a rolling basis. Email us at <a href='mailto:unicabinet@unicamp.org'>unicabinet@unicamp.org</a> to learn more.</p>
-    <a class='button' href='https://docs.google.com/forms/d/e/1FAIpQLSe_C-X9T38gxSzyTp40DKXGu1_0FQF7nrIthjnwJSobqXBHjw/viewform?c=0&w=1' style='display: inline-block; margin: auto; font-size: 1.3rem; text-align: center; width: auto; position: relative; left: 50%; transform: translateX(-50%); '>Apply online! &rarr;</a>
+    <a class='button' href='https://goo.gl/forms/6jWgFcoy3T9DhkrB2' style='display: inline-block; margin: auto; font-size: 1.3rem; text-align: center; width: auto; position: relative; left: 50%; transform: translateX(-50%); '>Apply online! &rarr;</a>
   </div>
 </section>
 <?php get_footer(); ?>
