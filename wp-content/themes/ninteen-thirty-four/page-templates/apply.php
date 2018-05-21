209d598e078bf47516ab63361d89be624f154280
Byron Lutz <byronlutz@gmail.com>
2018-05-21 16:06:02 -0700

    Restructured unicamp.org/apply

--- wp-content/themes/ninteen-thirty-four/page-templates/apply.php
+++ wp-content/themes/ninteen-thirty-four/page-templates/apply.php
@@ -8,16 +8,21 @@ class='photo-block'
 style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/campers-families/CampersFamilies.WhyCamp.jpg")'>
   <img class='photo-block__small-image' src='<? echo get_template_directory_uri(); ?>/images/photos/campers-families/CampersFamilies.WhyCamp.jpg' />
   <div class='photo-block__content'>
-    <h2>Volunteer with UCLA UniCamp!</h2>
-    <p class='explainer'>Applications for new and returning volunteers are now available!</p>
+    <h2>Volunteer with UCLA UniCamp</h2>
   </div>
 </section>
 
 <section class='row content-row'>
   <div class='medium-12 columns'>
+    <div class="callout primary">
+      <strong>Looking for camper applications?</strong> Go to <a href='https://apply.unicamp.org'>apply.unicamp.org</a> to learn more and submit an application online.
+    </div>
+    <br />
+
     <h5>Classic Camp</h5>
-    <p class='explainer'>Be a camp counselor this summer with UCLA UniCamp! Applications will be accepted on a rolling basis. Email us at <a href='mailto:unicabinet@unicamp.org'>unicabinet@unicamp.org</a> to learn more.</p>
-    <a class='button' href='https://goo.gl/forms/6jWgFcoy3T9DhkrB2' style='display: inline-block; margin: auto; font-size: 1.3rem; text-align: center; width: auto; position: relative; left: 50%; transform: translateX(-50%); '>Apply online! &rarr;</a>
+    <p class='explainer'>Volunteer applications have concluded for the season. If you'd like to be a volunteer next year or you're interested in our incoming freshman/transfer programs, <a href='http://eepurl.com/dvwCrL' target='_blank'>leave us your email address</a> and we'll send ymore information.</p>
+    <p class='explainer'>Questions? Email <a href='mailto:unicabinet@unicamp.org'>unicabinet@unicamp.org</a> for more information</p>
+    <a class='button' href='http://eepurl.com/dvwCrL' target='_blank'>Sign up for volunteer information &rarr;</a>
   </div>
 </section>
 <?php get_footer(); ?>
