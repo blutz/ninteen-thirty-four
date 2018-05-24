04933ed4322fdd38b81265b76c26c823b4de6bd2
Byron Lutz <byronlutz@gmail.com>
2018-05-24 12:17:28 -0700

    Added transfer program to /apply

--- wp-content/themes/ninteen-thirty-four/page-templates/apply.php
+++ wp-content/themes/ninteen-thirty-four/page-templates/apply.php
@@ -17,8 +17,15 @@ style='background-image: url("<? echo get_template_directory_uri(); ?>/images/ph
     <div class="callout primary">
       <strong>Looking for camper applications?</strong> Go to <a href='https://apply.unicamp.org'>apply.unicamp.org</a> to learn more and submit an application online.
     </div>
+  </div>
+  <div class='medium-6 columns'>
+    <h5>Accelerated Transfer Leadership Program</h5>
+    <p class='explainer'>Come up to camp as a transfer admit this summer, and then become a leader your first year at UCLA! Applications are due May 31.</p>
+    <p class='explainer'>Questions? Email <a href='mailto:fpandoy@unicamp.org'>fpandoy@unicamp.org</a> for more information</p>
+    <a class='button' href='https://docs.google.com/forms/d/e/1FAIpQLSc3wtWP1Gu374JalMHwORowCs7Y2BCllqKoyE8OKAiv7h8Quw/viewform'>Learn more and apply online &rarr;</a>
     <br />
-
+  </div>
+  <div class='medium-6 columns'>
     <h5>Classic Camp</h5>
     <p class='explainer'>Volunteer applications have concluded for the season. If you'd like to be a volunteer next year or you're interested in our incoming freshman/transfer programs, <a href='http://eepurl.com/dvwCrL' target='_blank'>leave us your email address</a> and we'll send ymore information.</p>
     <p class='explainer'>Questions? Email <a href='mailto:unicabinet@unicamp.org'>unicabinet@unicamp.org</a> for more information</p>
