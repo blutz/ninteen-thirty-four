ef2ab22f8e2fdbfd2b7a000bf9a747acb57f09dd
Byron Lutz <byronlutz@gmail.com>
2018-06-08 14:09:52 -0700

    Added SWD

---
+++ wp-content/themes/ninteen-thirty-four/page-templates/swd.php
@@ -0,0 +1,44 @@
+<?php /* Template Name: Students with Dependents */ ?>
+<?php get_header(); ?>
+
+<section
+class='photo-block'
+style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/campers-families/CampersFamilies.WhyCamp.jpg")'>
+  <img class='photo-block__small-image' src='<? echo get_template_directory_uri(); ?>/images/photos/campers-families/CampersFamilies.WhyCamp.jpg' />
+  <div class='photo-block__content'>
+    <h2>Students with Dependents Camp</h2>
+    <p class='explainer'>
+      Breaking from our traditional camp model, UCLA UniCamp invites UCLA students with dependents to enjoy a quick respite from the city&hellip;
+    </p>
+  </div>
+</section>
+
+<section class='row content-row'>
+  <div class='medium-12 columns'>
+    <p class='explainer'>
+   Pack some old clothes, grab a sleeping bag, put the family in the car, and join us for another amazing weekend at Camp River Glen. Hosted by UCLA UniCamp and it’s student volunteers, Students w/ Dependents Family Weekend is a chance for Bruins to get away from the busy city, relax in nature, breath fresh air, and share a camp experience with your family. UCLA UniCamp volunteers will staff program amenities of the campsite as you and your family can experience a traditional camp experience.
+    </p>
+  </div>
+  <div class='medium-6 columns'>
+    <h3>Registration</h3>
+    <p>Online registration will be available in the next few days.</p>
+    <a class='button' href='http://eepurl.com/dxEeZX' target='_blank'>Sign up to be notified when registration is available</a>
+  </div>
+  <div class='medium-6 columns'>
+    <p>
+      <h3>Other info</h3>
+      <ul>
+        <li>Adults to pay $50 to help cover cost of staffing and food. (If further financial assistnace is needed, please contact Jean at jlibby@saonet.ucla.edu)</li>
+        <li>All Children are FREE</li>
+        <li>Check-In begins at 1:00 PM on Friday June 20</li>
+        <li>Transportation to Camp River Glen will be required (Carpools will be set-up for those who need)</li>
+      </ul>
+    </p>
+    <p>
+      <strong>For more information contact Jean Libby at <a href='mailto:jlibby@saonet.ucla.edu'>jlibby@saonet.ucla.edu</a></strong>
+    </p>
+  </div>
+</section>
+
+
+<?php get_footer(); ?>
