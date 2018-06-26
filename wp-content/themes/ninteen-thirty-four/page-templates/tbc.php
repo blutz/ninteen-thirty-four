4fccce0b64d1bb2314d4e30ea93070299d5633b8
Byron Lutz <byronlutz@gmail.com>
2018-06-26 00:34:57 -0700

    True Bruin Camp Webpage

---
+++ wp-content/themes/ninteen-thirty-four/page-templates/tbc.php
@@ -0,0 +1,42 @@
+<?php /* Template Name: True Bruin Camp 2018 */ ?>
+<?php get_header(); ?>
+
+<meta name='unicamp-page' value='tbc' />
+
+<section
+class='photo-block'
+style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/programs/ocp.jpg")'>
+  <img class='photo-block__small-image' src='<? echo get_template_directory_uri(); ?>/images/photos/programs/ocp.jpg' />
+  <div class='photo-block__content'>
+    <h2>True Bruin Camp 2018</h2>
+    <p class='explainer'>Three days and two nights that just might put everything into perspective...</p>
+  </div>
+</section>
+
+<section class='row content-row'>
+  <div class='medium-12 columns' style='text-align: center'>
+    <h5>Apply online!</h5>
+    <a class='button' href='https://apply.unicamp.org/login/register/tbc18.1'>Register for Date 1 &rarr;</a>
+    <a class='button' href='https://apply.unicamp.org/login/register/tbc18.2'>Register for Date 2 &rarr;</a>
+    <a class='button' href='https://apply.unicamp.org/login/register/tbc18.3'>Register for Date 3 &rarr;</a>
+  </div>
+  <div class='medium-6 columns'>
+    <h5>Extend your UCLA Orientation</h5>
+    <p>Come to Camp River Glen, the home of the official charity of the students of UCLA. Leave from UCLA to embark on a three-day adventure where you'll get to know other incoming students. Spend some time in the mountains learning about who you are and who you might want to be. Planned and run by current students, the program will allow you to experience all of our camp activities while learning about the True Bruin Values of Respect, Accountability, Integrity, Service, and Excellence.</p>
+    <p>Learn about <em>you</em>. What impact do you want to make over the next four years? What are your goals? Get an early start on fulfilling your UCLA dreams by meeting diverse but like-minded individuals who want to make a change.</p>
+  </div>
+  <div class='medium-6 columns'>
+    <h5>Eligibility</h5>
+    <ul>
+      <li>All incoming UCLA students are welcome!</li>
+      <li>Not in one of the orientation sessions that coincide with a True Bruin Camp session? Feel free to sign up for any of the three available sessions. (Note: you will not be able to change your orientation session to go to this program. We can help arrange an extra night in Westwood before or after your orientation session if needed.)</li>
+    </ul>
+    <h5>Other info:</h5>
+    <ul>
+      <li>The cost for food, supplies, and transportation is $50 per student</li>
+      <li>Transportation from UCLA to Camp River Glen and back is provided. The program starts once your orientation session concludes, around 6pm. We will be back at UCLA two days later around 5pm.</li>
+      <li>Be sure to bring a sleeping bag, warm clothes, sunscreen, water bottle, and a flashlight</li>
+    </ul>
+  </div>
+</section>
+<?php get_footer(); ?>
