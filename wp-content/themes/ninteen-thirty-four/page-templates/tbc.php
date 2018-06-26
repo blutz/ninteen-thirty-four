80f4a6d2b6dcb1f725c009efd04350b919dea73f
Byron Lutz <byronlutz@gmail.com>
2018-06-26 11:32:05 -0700

    Updated TBC site with dates

--- wp-content/themes/ninteen-thirty-four/page-templates/tbc.php
+++ wp-content/themes/ninteen-thirty-four/page-templates/tbc.php
@@ -16,14 +16,21 @@ style='background-image: url("<? echo get_template_directory_uri(); ?>/images/ph
 <section class='row content-row'>
   <div class='medium-12 columns' style='text-align: center'>
     <h5>Apply online!</h5>
-    <a class='button' href='https://apply.unicamp.org/login/register/tbc18.1'>Register for Date 1 &rarr;</a>
-    <a class='button' href='https://apply.unicamp.org/login/register/tbc18.2'>Register for Date 2 &rarr;</a>
-    <a class='button' href='https://apply.unicamp.org/login/register/tbc18.3'>Register for Date 3 &rarr;</a>
+    <a class='button' href='https://apply.unicamp.org/login/register/tbc18.1'>Register for July 11-13 &rarr;</a>
+    <a class='button' href='https://apply.unicamp.org/login/register/tbc18.2'>Register for July 20-22 &rarr;</a>
+    <a class='button' href='https://apply.unicamp.org/login/register/tbc18.3'>Register for August 12-14 &rarr;</a>
   </div>
   <div class='medium-6 columns'>
     <h5>Extend your UCLA Orientation</h5>
     <p>Come to Camp River Glen, the home of the official charity of the students of UCLA. Leave from UCLA to embark on a three-day adventure where you'll get to know other incoming students. Spend some time in the mountains learning about who you are and who you might want to be. Planned and run by current students, the program will allow you to experience all of our camp activities while learning about the True Bruin Values of Respect, Accountability, Integrity, Service, and Excellence.</p>
     <p>Learn about <em>you</em>. What impact do you want to make over the next four years? What are your goals? Get an early start on fulfilling your UCLA dreams by meeting diverse but like-minded individuals who want to make a change.</p>
+    <h5>Sessions</h5>
+    <p>There are three True Bruin Camp sessions during the 2018 summer:</p>
+    <ul>
+      <li><strong>July 11-13</strong> <a href='https://apply.unicamp.org/login/register/tbc18.1'>(register)</a> — after orientation session 101.</li>
+      <li><strong>July 20-22</strong> <a href='https://apply.unicamp.org/login/register/tbc18.2'>(register)</a> — after orientation session 102.</li>
+      <li><strong>August 12-14</strong> <a href='https://apply.unicamp.org/login/register/tbc18.3'>(register)</a> — before orientation session 106. This session will come back to LA the night of August 14. You can request an early arrival from <a href='http://newstudents.ucla.edu/orientation-programs/first-year-students/'>New Student & Transition Programs</a>.</li>
+    </ul>
   </div>
   <div class='medium-6 columns'>
     <h5>Eligibility</h5>
