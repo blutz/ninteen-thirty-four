eddd7e8e1879fbbff9af798437f4200c9be98175
Byron Lutz <byronlutz@gmail.com>
2018-07-02 01:05:40 -0700

    Made TBC edits to reflect email

--- wp-content/themes/ninteen-thirty-four/page-templates/tbc.php
+++ wp-content/themes/ninteen-thirty-four/page-templates/tbc.php
@@ -22,28 +22,30 @@ style='background-image: url("<? echo get_template_directory_uri(); ?>/images/ph
   </div>
   <div class='medium-6 columns'>
     <h5>Extend your UCLA Orientation</h5>
-    <p>Come to Camp River Glen, the home of the official charity of the students of UCLA. Leave from UCLA to embark on a three-day adventure where you'll get to know other incoming students. Spend some time in the mountains learning about who you are and who you might want to be. Planned and run by current students, the program will allow you to experience all of our camp activities while learning about the True Bruin Values of Respect, Accountability, Integrity, Service, and Excellence.</p>
-    <p>Learn about <em>you</em>. What impact do you want to make over the next four years? What are your goals? Get an early start on fulfilling your UCLA dreams by meeting diverse but like-minded individuals who want to make a change.</p>
+    <p>Come to Camp River Glen, the home of UCLA's official charity, UniCamp. Leave from UCLA to embark on a three-day adventure where you'll get to know other incoming students and spend some time in the mountains learning about who you are and who you want to become. Planned and run by current UCLA students, the program will allow you to experience all of our camp activities while learning about the True Bruin Values of Respect, Accountability, Integrity, Service, and Excellence.</p>
+    <p>What impact do you want to make over the next four years? What are your goals? True Bruin Camp will help you get an early start on fulfilling your UCLA dreams by meeting diverse but like-minded individuals who want to make a change.</p>
+    <p>UniCamp engages 500 UCLA student volunteers in providing a camp experience to over 1200 Los Angeles area urban youth ages 10-14.  Once you begin here in the Fall, consider applying to be a volunteer for UniCamp!  For more information, check back at www.unicamp.org in Fall.</p>
     <h5>Sessions</h5>
     <p>There are three True Bruin Camp sessions during the 2018 summer:</p>
     <ul>
-      <li><strong>July 11-13</strong> <a href='https://apply.unicamp.org/login/register/tbc18.1'>(register)</a> — after orientation session 101.</li>
-      <li><strong>July 20-22</strong> <a href='https://apply.unicamp.org/login/register/tbc18.2'>(register)</a> — after orientation session 102.</li>
-      <li><strong>August 12-14</strong> <a href='https://apply.unicamp.org/login/register/tbc18.3'>(register)</a> — before orientation session 106. This session will come back to LA the night of August 14. You can request an early arrival from <a href='http://newstudents.ucla.edu/orientation-programs/first-year-students/'>New Student & Transition Programs</a>.</li>
+      <li><strong>July 11-13</strong> <a href='https://apply.unicamp.org/login/register/tbc18.1'>(register)</a> — after New Student Orientation (NSO) session 101.</li>
+      <li><strong>July 20-22</strong> <a href='https://apply.unicamp.org/login/register/tbc18.2'>(register)</a> — after New Student Orientation session 102.</li>
+      <li><strong>August 12-14</strong> <a href='https://apply.unicamp.org/login/register/tbc18.3'>(register)</a> — before New Student Orientation session 106. This session will come back to Los Angeles the night of August 14. You can request an early arrival from <a href='http://newstudents.ucla.edu/orientation-programs/first-year-students/'>New Student & Transition Programs</a> for an added $50 to spend the night at UCLA before your NSO session.</li>
     </ul>
   </div>
   <div class='medium-6 columns'>
     <h5>Eligibility</h5>
     <ul>
       <li>All incoming UCLA students are welcome!</li>
-      <li>Not in one of the orientation sessions that coincide with a True Bruin Camp session? Feel free to sign up for any of the three available sessions. (Note: you will not be able to change your orientation session to go to this program. We can help arrange an extra night in Westwood before or after your orientation session if needed.)</li>
+      <li>Not in one of the New Student Orientation sessions that coincide with a True Bruin Camp session? You can still sign up for True Bruin Camp, but please note that you will not be able to change your NSO session date to coincide with the date of your True Bruin Camp. (Note: We can help arrange an extra night in Westwood before or after your NSO session if needed.)</li>
     </ul>
     <h5>Other info:</h5>
     <ul>
       <li>The cost for food, supplies, and transportation is $50 per student</li>
-      <li>Transportation from UCLA to Camp River Glen and back is provided. The program starts once your orientation session concludes, around 6pm. We will be back at UCLA two days later around 5pm.</li>
+      <li>Transportation from UCLA to Camp River Glen and back is provided. The program starts once your New Student Orientation session concludes, around 6pm. We will be back at UCLA two days later around 5pm.</li>
       <li>Be sure to bring a sleeping bag, warm clothes, sunscreen, water bottle, and a flashlight</li>
     </ul>
+    <p><strong>Questions? Email <a href='mailto:registration@unicamp.org'>registration@unicamp.org</a></strong></p>
   </div>
 </section>
 <?php get_footer(); ?>
