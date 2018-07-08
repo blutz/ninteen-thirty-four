14bfd80caab4056ee5b47284ec9626228411c96f
Byron Lutz <byronlutz@gmail.com>
2018-07-08 10:42:20 -0700

    Valley fire page for staff

---
+++ wp-content/themes/ninteen-thirty-four/page-templates/valley-fire.php
@@ -0,0 +1,67 @@
+<?php /* Template Name: Valley Fire 2018 */ ?>
+<?php get_header(); ?>
+
+<meta name='unicamp-page' value='tbc' />
+
+<section
+class='photo-block'
+style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/programs/ocp.jpg")'>
+  <img class='photo-block__small-image' src='<? echo get_template_directory_uri(); ?>/images/photos/programs/ocp.jpg' />
+  <div class='photo-block__content'>
+    <h2>Valley Fire 2018</h2>
+  </div>
+</section>
+
+<section class='row content-row'>
+  <div class='medium-6 medium-offset-3 columns end'>
+    <p>UCLA UniCamp operates at Camp River Glen in Barton Flats, on USFS Road 1N45, between Glass Road and Highway 38.</p>
+    <p>We have camp sessions starting on:</p>
+    <ul>
+      <li>Tuesday, July 10</li>
+      <li>Wednesday, July 11</li>
+      <li>Sunday, July 15</li>
+      <li>Monday July 16</li>
+      <li>And more &hellip; (this list will be updated if the road closure continues past the 16th)</li>
+    </ul>
+    <p>Campers, employees, and volunteers will be joining us at camp and will need to access camp through Highway 38. They have been instructed to go to Big Bear through the Bryant Street closure. The following is a list of full-time camp staff:</p>
+    <div class='row'>
+      <div class='medium-6 columns'>
+        <ul>
+          <li>Kaitlyn Akiyama</li>
+          <li>Adam DeBrosse</li>
+          <li>Leslie Gutierrez</li>
+          <li>Luke Higbee</li>
+          <li>Angeline Jiang</li>
+          <li>Maninder Kaur</li>
+          <li>Stephanie Ko</li>
+          <li>Jason Liou</li>
+          <li>Lauren Lopez</li>
+          <li>Byron Lutz</li>
+        </ul>
+      </div>
+      <div class='medium-6 columns'>
+        <ul>
+          <li>Martin Mai</li>
+          <li>Jacob Moulton</li>
+          <li>Alyx Navarro</li>
+          <li>Franklin Pandoy</li>
+          <li>Parth Prashar</li>
+          <li>Christine Rosales</li>
+          <li>Samantha Valdarrago</li>
+          <li>Don Wellington</li>
+          <li>Vincent Wong</li>
+        </ul>
+      </div>
+    </div>
+    <p>Other volunteers, campers, and staff in private cars, vans, or charter busses will also be joining us. If you need to verify that anyone's affiliation with UCLA UniCamp / Camp River Glen, you can contact us at:</p>
+    <ul>
+      <li><strong>Call our main number to reach the first available person</strong> — <a href='tel:3102088252,767'>310-208-8252 ext. 767</a></li>
+      <li><strong>Byron Lutz</strong> (Camp Director) — <a href='tel:9163901993'>916-390-1993</a> or <a href='tel:3107309015'>310-730-9015</a></li>
+      <li><strong>Jason Liou</strong> (Interim Executive Director) — <a href='tel:3103849489'>310-384-9489</a> or <a href='tel:3107309012'>310-730-9489</a></li>
+      <li><strong>Camp River Glen Landline</strong> — <a href='tel:9093899442'>909-389-94442</a></li>
+    </ul>
+    <p>You can verify our organized camp location/permit by contacting the San Bernardino County of Public Health at <a href='tel:8004422283'>800-442-2283</a>. A copy of our permit is included below.</p>
+    <img src='https://www.unicamp.org/wp-content/uploads/2018/07/permit2018.jpg' />
+  </div>
+</section>
+<?php get_footer(); ?>
