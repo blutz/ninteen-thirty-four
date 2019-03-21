311b437d496fc98c139424a391aa0c7fa478b8b6
Byron Lutz <byronlutz@gmail.com>
2019-03-20 20:57:37 -0700

    Added summer page (prereq to updating the homepage)

---
+++ wp-content/themes/ninteen-thirty-four/page-templates/summer.php
@@ -0,0 +1,143 @@
+<?php /* Template Name: Summer Info */ ?>
+<?php get_header(); ?>
+
+<meta name='unicamp-page' value='dcfs' />
+
+<section
+class='photo-block'
+style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/programs/classic-camp.jpg")'>
+  <img class='photo-block__small-image' src='<? echo get_template_directory_uri(); ?>/images/photos/programs/classic-camp.jpg' />
+  <div class='photo-block__content'>
+    <h2>Camper Registration 2019</h2>
+    <p class='explainer'>Come to UCLA UniCamp this summer! Applications will be available online soon.</p>
+  </div>
+</section>
+
+<section class='row content-row'>
+  <div class='medium-6 medium-offset-3 columns end'>
+    <p class='explainer' style='text-align: center'>Get an email when applications are released:</p>
+
+
+<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://unicamp.us13.list-manage.com/subscribe/post?u=0fcb41f54ca069df3bc85f56c&amp;id=757e4f36c1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<strong>Email Address </strong>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+	<input type="hidden" value="" name="DCFS" value="Yes">
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0fcb41f54ca069df3bc85f56c_757e4f36c1" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+<!--End MailChimp Signup -->
+
+    <hr />
+    <p style='text-align: center;'><em><strong>Questions?</strong> Email us at <a href='mailto:registration@unicamp.org'>registration@unicamp.org</a> or&nbsp;call&nbsp;us&nbsp;at&nbsp;<a href='tel:3102088252,1'>310&#8209;208&#8209;8252&nbsp;ext&nbsp;1</a>.</em></p>
+  </div>
+
+  <div class='medium-8 columns'>
+    <h4>Session Dates</h4>
+    <p>Each session has a different theme. Public availability may be limited for some sessions that are developed in conjunction with community organizations.</p>
+    <ul>
+      <li>June 22-28: <strong>Transitional Youth Empowerment (Foster Youth)</strong></li>
+      <li>June 28 - July 3: <strong>Camp Comforting Hearts (Bereavement)</strong></li>
+      <li>July 8-14: <strong>The Village Session (African American Youth Empowerment)</strong></li>
+      <li>July 14-21: <strong>Leadership Development &amp; Life Skills</strong></li>
+      <li>July 21-28: <strong>Health, Nutrition, &amp; Fitness</strong></li>
+      <li>July 28 - August 3: <strong>AcademiCamp (Social/Emotional Learning)</strong></li>
+      <li>August 3-9: <strong>Outdoor Environmental Science</strong></li>
+    </ul>
+    <h4>Registration Process</h4>
+    <p>Registration is all online and may be completed from a phone, tablet, or computer. An email address is required and is used for communicating information about your sesion. (You can get a free email address from <a href='https://www.google.com/gmail/about/'>Gmail</a> and many others.) An adult (18+) parent/guardian is also required to finalize enrollment.</p>
+    <p>In addition to basic information, the application also requires:</p>
+    <ul>
+      <li>Contact info for 2-3 emergency contacts</li>
+      <li>Camper's health history (including allergies and medications)</li>
+      <li>Camper's immunization record</li>
+      <li>Camper's health insurance and medical provider information</li>
+      <li>Income information for everyone in the household</li>
+    </ul>
+    <p>Gather this information as well as a credit/debit card for online payment to finish the registration process faster.</p>
+    <p>If you are missing any information, contact us at <a href='mailto:registration@unicamp.org'>registration@unicamp.org</a> and we can work with you to complete the application.</p>
+    <h4>Packing List</h4>
+    <p>Here is a packing list for an 8-day/7-night session. You may want to adjust the quantities slightly depending on your session length. The temperature at camp can range from 40 degrees at night to over 90 degrees during the day.</p>
+    <ul class='summer__packing-list'>
+      <li>Sack lunch (for the first day)</li>
+      <li><strong>Clothes</strong>
+        <ul>
+          <li>7 pairs of underwear</li>
+          <li>7 pairs of socks</li>
+          <li>3-4 pairs of durable pants</li>
+          <li>3-4 pairs of shorts</li>
+          <li>7 t-shirts</li>
+          <li>1 jacket</li>
+          <li>2 sweatshirts/sweaters</li>
+          <li>1 swim suit</li>
+          <li>1 pair <strong>CLOSED TOE</strong> shoes</li>
+          <li>Pajamas</li>
+          <li>Hat</li>
+          <li>Sunglasses</li>
+        </ul>
+      </li>
+      <li>2 towels</li>
+      <li>1 water bottle</li>
+      <li>1 cheap/disposable camera (optional)</li>
+      <li>Flashlight with extra batteries</li>
+      <li>Pillow</li>
+      <li>Warm sleeping bag</li>
+      <li>Medications in original packaging</li>
+      <li><strong>Toiletries</strong>
+        <ul>
+          <li>Unscented soap</li>
+          <li>Toothbrush &amp; toothpaste</li>
+          <li>Comb, brush, or other hair care</li>
+          <li>Unscented shampoo</li>
+          <li>Unscented deodorant</li>
+          <li>Sunscreen</li>
+          <li>Bug spray</li>
+          <li>Lip balm</li>
+        </ul>
+      </li>
+    </ul>
+    <p>Please limit yourself to 2 pieces of luggage per camper and label your bags and anything else that may get lost (e.g. water bottles, flashlights, etc.). Any medication will be distributed by our nurse. Epi-Pens, rescue inhalers, and any other emergency medication will kept near the camper at all times.</p>
+    <p><strong>What <em>not</em> to bring.</strong> Know what your child is bringing! Any of these items may earn them an early trip home.</p>
+    <ul>
+      <li><strong>Food or things that smell like food.</strong> Since we are in the forest, we are very careful about not attracting animals. Except for the first day's sack lunch, any type of food or food-scented items are not allowed. Campers will have a chance to throw away any leftover food once they arrive.</li>
+      <li><strong>Weapons, fireworks, drugs, alcohol, controlled substances, tabacco, smoking/vaping products, matches, lighters, etc.</strong> Even if legal, these are not appropriate for our camp environment and some have the potential to cause forest fires.</li>
+      <li><strong>Toys.</strong> Small stuffed friends are okay, but there is enough activity going on each day that toys aren't necessary and can be distracting.</li>
+      <li><strong>Video games, music players, cell phones, and other expensive items.</strong> These items take away from the camp experience and may be damaged by the active, outdoor nature of camp. Cell phones will be collected on the first day and stored safely until the end of the session.</li>
+      <li><strong>Markers or marking pens.</strong> Any supplies for arts &amp; crafts or journaling activities will be available at camp.</li>
+      <li><strong>Revealing clothing, clothing with profanity, clothing with drug/alcohol references, etc.</strong> If a camper is seen wearing anything inappropriate, they will be asked to change. Appropriate dress helps us maintain a positive, encouraging environment at camp.</li>
+      <li><strong>Pets.</strong> Please contact us if the camper needs to bring a service animal so we can ensure the safety of the animal and the other campers.</li>
+    </ul>
+  </div>
+  <div class='medium-4 columns'>
+    <h4>Transportation</h4>
+    <p>UCLA UniCamp runs our programs at our campsite in the San Bernardino National Forest. Open enrollment sessions include bus transportation from Los Angeles to/from our campsite. The exact location in Los Angeles will be confirmed with you over email prior to your session. Pickup locations for most sessions are at UCLA.</p>
+    <p>Busses pick up campers around 9am on the first day of the session and drop off campers around 5pm on the last day.</p>
+    <p><strong>Pickup times and locations vary by session and will be confirmed with you over email after registration.</strong></p>
+    <h4>Cost</h4>
+    <p>Our direct cost per camper is $600. However, through sponsorships, grants, and fundraising, we are able to lower the cost families pay. For open enrollment, the cost per camper is:</p>
+    <ul>
+      <li><strong>$199</strong> if qualified for free/reduced lunch</li>
+      <li><strong>$300</strong> if not qualified</li>
+    </ul>
+    <p>We ask all families to complete an income eligibility form as part of the registration process. The forms qualify us for additional funding that helps us reduce the cost to you.</p>
+    <p><strong>Campers with an active case with Los Angeles DCFS may qualify for a full campership.</strong> Contact us or have your social worker contact us at <a href='mailto:registration@unicamp.org'>registration@unicamp.org</a> for more information.</p>
+    <h4>Other Information</h4>
+    <p><strong>Can I send letters?</strong> Please drop off any letters (labeled with the camper's name and date you'd like to give it to them) with UniCamp staff on the first day of camp. Small care packages are okay as long as they do not have food.</p>
+    <p><strong>Can I contact my camper?</strong> If you have an emergency and need to get in contact with your camper, call us at 310-208-8252 ext 767 (easily remembered as 844-UNICAMP ext SOS). This number is only connected during our summer season. We will reach out to you directly if we have any questions or if there are any emergencies at camp. Since camper independence is one of our major outcomes (and in some cases can cause homesickness), we strongly discourage phone calls to campers.</p>
+    <p><strong>What are the age requirements?</strong> Campers should be between 10 and 17 years old to participate in UCLA UniCamp.</p>
+    <p><strong>What are the sleeping arrangements?</strong> Campers will sleep in a cabin with up to 9 other similarly-aged campers and 2 counselors of the same gender. Cabins are open air and have a bathroom and sink.</p>
+    <p><strong>How are campers supervised?</strong> All UCLA UniCamp counselors are volunteers who go through four months of training and are overseen by more experienced counselors and UniCamp full-time staff. We follow the "rule of three" where campers are always with two or more people, at least one of whom must be an adult. We have an average supervision ratio of 3 campers to 1 counselor.</p>
+  </div>
+</section>
+<?php get_footer(); ?>
