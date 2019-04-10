604aec0c62d83b005d641df40510c851d5e8a126
Byron Lutz <byronlutz@gmail.com>
2019-04-09 20:22:55 -0700

    Restore River Glen landing page

---
+++ wp-content/themes/ninteen-thirty-four/page-templates/restore-river-glen.php
@@ -0,0 +1,53 @@
+<?php /* Template Name: Restore River Glen */ ?>
+<?php get_header(); ?>
+
+<style type='text/css'>
+  .emergency {
+    font-weight: bold;
+    color: #C3112F;
+  }
+</style>
+
+<meta name='unicamp-page' value='dcfs' />
+
+<section
+class='photo-block'
+style='background-image: url("<? echo get_template_directory_uri(); ?>/images/photos/home/restoreriverglen.jpg")'>
+  <img class='photo-block__small-image' src='<? echo get_template_directory_uri(); ?>/images/photos/home/restoreriverglen.jpg' />
+  <div class='photo-block__content'>
+    <h2>Flash Flood!<br />Help Restore Camp River Glen for the Campers!</h2>
+  </div>
+</section>
+
+<section class='row content-row'>
+  <div class='medium-8 medium-offset-2 columns'>
+    <p><strong>Angeles Oaks, CA</strong> - On February 14th, heavy rains in the San Bernardino Mountains caused major flash floods, which tore through fire-damaged areas. The US Forest Service is calling this a 50-year flood event, noting that this is the worst flood any of them have ever seen. On February 21st, California Governor Gavin Newsom declared a state of emergency in 21 counties, including San Bernardino County where our camp resides. We have made several trips to our campsite, Camp River Glen, to survey the damage and each time, we discovered even more damage.</p>
+    <p>The main road into Camp River Glen has become inaccessible due to extensive and severe erosion. Camp River Glen itself has taken moderate damage to areas directly adjacent to the Santa Ana River where the flood was most intense, including our bridge, pump house, and water system. There is moderate erosion to the campgrounds and trails within Camp River Glen.</p>
+    <p class='emergency'>We estimate that total damages are in excess of $50,000.</p>
+    <p>We are working with several county, state, and federal agencies to reestablish primary access to Camp River Glen. Concurrently, we will begin campsite repairs. <strong><em>This is where we need your help!</em></strong></p>
+    <p><strong>We have 8 weeks to raise $50,000 to save the summer camp season for 1,000 campers! If you can’t give monetarily, please consider a gift of your professional services (e.g. engineering), or supplies. Please give generously and help us spread the word!</strong></p>
+    <p><strong>More than 1,000 kids are hanging their summer camp hopes on us….let’s make this happen!</strong></p>
+    <p style='text-align: center'>
+      <a href='https://donate.unicamp.org/checkout/731' class='button' style='font-size: 1.3em; margin: 1em;'>Donate Online</a>
+    </p>
+    <hr />
+    <h5>Work Weekends</h5>
+    <p>We will be hosting work weekends to excavate silt and mud-buried areas of camp, remove fallen trees and debris, and rebuild trails. We have multiple weekends planned and there is so much to do.</p>
+    <p style='text-align: center'>
+      <a href='https://docs.google.com/forms/d/e/1FAIpQLSeY9Vvq2MaHjr4JDjM6tVGUBsqVeebjkIu5nphWb_vn4kCgrQ/viewform?vc=0&c=0&w=1' class='button'>Sign up for Work Weekends</a>
+    </p>
+    <p>We are also in need of equipment and services to help rebuild Camp River Glen including:</p>
+    <ul style='columns: 3'>
+      <li>Shovels</li>
+      <li>Rakes</li>
+      <li>Work gloves</li>
+      <li>Chainsaws</li>
+      <li>Wheelbarrows</li>
+      <li>Shop vacuum</li>
+      <li>Leaf blower</li>
+      <li>Volunteer tractor operators</li>
+      <li>Pro bono engineering services</li>
+    </ul>
+  </div>
+</section>
+<?php get_footer(); ?>
