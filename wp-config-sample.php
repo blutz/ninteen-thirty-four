c37023c2321517671adeae80a1f1a13fae9be111
Byron Lutz <byronlutz@gmail.com>
2019-10-23 19:53:24 -0700

    Upgrade compose version and help local setup

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -37,6 +37,9 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+//define('WP_HOME','http://localhost:8252');
+//define('WP_SITEURL','http://localhost:8252');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
