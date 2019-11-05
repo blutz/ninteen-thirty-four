3623557a4a7192c204520d692e03f15ad72c34c4
Byron Lutz <byronlutz@gmail.com>
2019-11-04 17:58:17 -0800

    Set up site structure with breakpoints

--- watch-sass.sh
+++ watch-sass.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # NOTE: Run this on your local machine!! And the web container must be running
-retry -t 0 -f='say failed' 'find wp-content/themes/ninteen-thirty-four/sass | entr docker-compose exec web ./build-sass.sh'
+retry -t 0 -f 'say failed' 'find wp-content/themes/ninteen-thirty-four/sass -name "*.scss" | entr docker-compose exec web ./build-sass.sh'
