b0ae699b47d4eb9829ef491047b5bd0c32d1b139
Byron Lutz <byronlutz@gmail.com>
2019-12-26 17:50:17 -0800

    404 page

--- wp-content/themes/ninteen-thirty-four/404.php
+++ wp-content/themes/ninteen-thirty-four/404.php
@@ -15,39 +15,14 @@ get_header();
 
       <section class="error-404 not-found">
         <header class="page-header">
-          <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ninteen-thirty-four' ); ?></h1>
+          <h1 class="page-title"><?php esc_html_e( "Sorry, there's nothing here", 'ninteen-thirty-four' ); ?></h1>
         </header><!-- .page-header -->
 
         <div class="page-content">
-          <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ninteen-thirty-four' ); ?></p>
+          <p>🙁<?php esc_html_e( 'Content not found (error 404). Check your spelling or email registration@unicamp.org for help.', 'ninteen-thirty-four' ); ?></p>
 
           <?php
           get_search_form();
-
-          the_widget( 'WP_Widget_Recent_Posts' );
-          ?>
-
-          <div class="widget widget_categories">
-            <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'ninteen-thirty-four' ); ?></h2>
-            <ul>
-              <?php
-              wp_list_categories( array(
-                'orderby'    => 'count',
-                'order'      => 'DESC',
-                'show_count' => 1,
-                'title_li'   => '',
-                'number'     => 10,
-              ) );
-              ?>
-            </ul>
-          </div><!-- .widget -->
-
-          <?php
-          /* translators: %1$s: smiley */
-          $ninteen_thirty_four_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'ninteen-thirty-four' ), convert_smilies( ':)' ) ) . '</p>';
-          the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$ninteen_thirty_four_archive_content" );
-
-          the_widget( 'WP_Widget_Tag_Cloud' );
           ?>
 
         </div><!-- .page-content -->
