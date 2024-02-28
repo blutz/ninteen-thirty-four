9f62669910247de063bc28056942bb1161bae02d
Byron Lutz <byronlutz@gmail.com>
2024-02-28 01:11:27 -0800

    Fix corrupted block messages

--- unicamp-blocks/build/hashtabs/index.asset.php
+++ unicamp-blocks/build/hashtabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => 'a048e5e9c708114df84d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => 'a159912043cfd1415d3f');
