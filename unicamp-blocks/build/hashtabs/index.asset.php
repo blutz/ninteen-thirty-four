6db3babfb061fb37b78f9db42aecda388e82349c
Byron Lutz <byronlutz@gmail.com>
2024-02-28 02:53:26 -0800

    Added a little more style polish to frontend and editor

--- unicamp-blocks/build/hashtabs/index.asset.php
+++ unicamp-blocks/build/hashtabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => 'dd3e34c56689e389cd0e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => '4dab57b72187083184f2');
