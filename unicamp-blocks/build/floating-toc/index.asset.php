c9a562be1b7c781b1eed4b261df34bdef957e3fa
Byron Lutz <byronlutz@gmail.com>
2025-06-04 18:22:18 -0700

    Deploy floating ToC changes

--- unicamp-blocks/build/floating-toc/index.asset.php
+++ unicamp-blocks/build/floating-toc/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => '70a46b4cd346d6db84ff');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data'), 'version' => '74e6a3ff1ac7995867af');
