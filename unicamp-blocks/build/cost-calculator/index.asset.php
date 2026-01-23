51d9890ad55703de51aed255814f92350035aaed
Byron Lutz <byronlutz@gmail.com>
2026-01-22 18:11:36 -0800

    Build

--- unicamp-blocks/build/cost-calculator/index.asset.php
+++ unicamp-blocks/build/cost-calculator/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '837a18600c6a0fcc2aa0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6f7704397cac60bad9f2');
